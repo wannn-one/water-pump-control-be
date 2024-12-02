316387a80630899837a9e10344c803633addf932
Ikhwanul Abiyu <ikhwanulabiyu@gmail.com>
2024-12-02 07:24:18 +0700

    fix: cors issue

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: '*',
+        methods: ['GET,POST,PUT,DELETE'],
+        allowedHeaders: ['Content-Type,Authorization']
+    }
+));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
